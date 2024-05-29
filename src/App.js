05d16bea0e3939af18e78019067802a0ae7fb9fc
o6ez9na <f1stingday@gmail.com>
2024-05-29 17:24:20 +0300

    page layout

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React from 'react';
-import {
-  ChakraProvider,
-  Box
-} from '@chakra-ui/react';
+import { Center, ChakraProvider, Box, Input, Text } from '@chakra-ui/react';
 
 function App() {
   return (
     <ChakraProvider>
-      <Box
-      backgroundColor={'#2f3231'}      
-      height={'100vh'}
-      color={'#82807a'}
-      >
-        <h1>Hello World</h1>
+      <Box backgroundColor={'#2f3231'} height={'100vh'} color={'#82807a'}>
+        <Center>
+          <Text mt={'30vh'} fontSize={45}>
+            Hello World
+          </Text>
+        </Center>
+        <Center>
+          <Box width={'20vh'}>
+            <Input borderColor={'black'} placeholder="@tag"></Input>
+            <Input
+              mt={'3px'}
+              borderColor={'black'}
+              placeholder="@password"
+            ></Input>
+          </Box>
+        </Center>
       </Box>
     </ChakraProvider>
   );
